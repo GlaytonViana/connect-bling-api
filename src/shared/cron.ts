fe05768f57e2102f0e2e6d4c298bf5ed82bd34e1
Glayton Viana <desenvolvimento1@ineosistemas.com.br>
2022-01-27 08:00:45 -0300

    updated cron

--- src/shared/cron.ts
+++ src/shared/cron.ts
@@ -12,19 +12,33 @@ app.use(express.json())
 app.use(cors())
 
 cron.schedule('*/10 * * * *', async () => {
-    console.log('Executando cron ...')
+    console.log('Executando chamadas a API')
 
-    await axios.get(`${process.env.API_URL}/products`).catch(error => {
+    const api = axios.create({
+        baseURL: process.env.API_URL,
+        timeout: 1000 * 60 * 5,
+        responseType: 'json',
+    })
+
+    console.log('Get products')
+
+    await api.get('/products').catch(error => {
         console.log(error)
     })
 
-    await axios.get(`${process.env.API_URL}/orders`).catch(error => {
+    console.log('Get orders')
+
+    await api.get('/orders').catch(error => {
         console.log(error)
     })
 
-    await axios.get(`${process.env.API_URL}/bills`).catch(error => {
+    console.log('Get bills')
+
+    await api.get('/bills').catch(error => {
         console.log(error)
     })
+
+    console.log('cron executado')
 })
 
 app.listen(3334, () => {
